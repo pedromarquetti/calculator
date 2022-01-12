3e65fb9e9bcb2fa1ed777f919e181abb07591324
PedroMarquetti <pedro.marquettti@gmail.com>
2022-01-12 09:54:39 -0300

    remove unnecessary code

--- files/scrips.js
+++ files/scrips.js
@@ -1,8 +1,10 @@
 const calculator = document.querySelector('.calculator')
+const display_main = document.querySelector(".calculator__display__main")
 const keys = calculator.querySelector(".calculator__keys")
 const display = document.querySelector(".calcs")
 const curr = document.querySelector(".curr")
 const previousKeyType = calculator.dataset.previousKeyType
+// const btn = document.createElement("BUTTON",class="scroll")
 let calc = new Array()
 
 keys.addEventListener("click", e =>{
@@ -16,32 +18,20 @@ keys.addEventListener("click", e =>{
     };
     let calculate = (n1,op,n2) => { // calculator
         switch (op) {
-            case "+":  
-                console.log(typeof op);
-                let calc1 = parseFloat(n1) + parseFloat(n2)
-                console.log("calc ",`${parseFloat(n1)} ${op} ${parseFloat(n2)} = `,calc1)
-                console.log("sum");
+            case "+":                  
+                let calc1 = parseFloat(n1) + parseFloat(n2)                                
                 return calc1.toString();
                 
-            case "-":
-                console.log(typeof op);
-                let calc2 = parseFloat(n1) - parseFloat(n2)
-                console.log("calc ",`${parseFloat(n1)} ${op} ${parseFloat(n2)} = `,calc2)
-                console.log("subtrac");
+            case "-":                
+                let calc2 = parseFloat(n1) - parseFloat(n2)                                
                 return calc2.toString();
                     
-            case "*": case "×":
-                console.log(typeof op);
-                let calc3 = parseFloat(n1) * parseFloat(n2)
-                console.log("calc ",`${parseFloat(n1)} ${op} ${parseFloat(n2)} = `,calc3)
-                console.log("multi");
+            case "*": case "×":                
+                let calc3 = parseFloat(n1) * parseFloat(n2)                                
                 return calc3.toString();
                 
-            case "/": case "÷":
-                console.log(typeof op);
-                let calc4 = parseFloat(n1) / parseFloat(n2)
-                console.log("calc ",`${parseFloat(n1)} ${op} ${parseFloat(n2)} = `,calc4)
-                console.log("divide");
+            case "/": case "÷":                
+                let calc4 = parseFloat(n1) / parseFloat(n2)                                
                 return calc4.toString();
             default:
                 break;
@@ -60,7 +50,7 @@ keys.addEventListener("click", e =>{
         let exp = /[+]|[-]/;
         return exp.test(val)
     };
-    
+
     if (e.target.matches("button")){
         const key = e.target 
         const action = key.dataset.action 
@@ -69,24 +59,18 @@ keys.addEventListener("click", e =>{
         const currnum = curr.innerHTML 
         const previousKey = display.textContent[display.textContent.length -1]
 
-        if (displayednum === "" && (action === "divide"|| action === "multiply")){
-            console.error("no */ on  empty");
+        if (displayednum === "" && (action === "divide"|| action === "multiply")){            
         } else if (!action) { //if num is pressed
-            calculator.dataset.previousKeyType = "num"
-            console.warn("num pressed, appending to str: ");
-            console.log(`current key: ${keycontent}, prev key: ${previousKey}`);
+            calculator.dataset.previousKeyType = "num"                        
             display.append(keycontent)
             calc.push(keycontent)
             // let str = calc.join("")
-            // console.log("current str: ",str);
-        } else if (action && calculator.dataset.previousKeyType === "num" && action != "clear" && action != "calculate") {
-            console.warn("action pressed ");
-            console.log(`current key: ${keycontent}, prev key: ${previousKey}`);
+        // 
+        } else if (action && calculator.dataset.previousKeyType === "num" && action != "clear" && action != "calculate") {                        
             calculator.dataset.previousKeyType = "action"
             display.append(keycontent)
             calc.push(keycontent)
-        } else if ((action === "subtract" || action === "add") && multiDiv(previousKey) ){
-            console.warn("5/* -+5");
+        } else if ((action === "subtract" || action === "add") && multiDiv(previousKey) ){            
             calculator.dataset.previousKeyType = "action"
             display.append(keycontent)
             calc.push(keycontent)
@@ -95,13 +79,10 @@ keys.addEventListener("click", e =>{
             display.textContent = ""
             curr.textContent = ""
             calculator.dataset.previousKeyType = ""
-            calc = []
-            console.clear()
-            console.warn("cleared");
+            calc = []                        
 
         }
-        let str = calc.join("")
-        console.log("str outside loop",str);
+        let str = calc.join("")        
 
 
         if (isCalc(str)) { // if i is calc:
@@ -110,8 +91,7 @@ keys.addEventListener("click", e =>{
             // .filter(r => {return r.length !=0 }) // filters "" out of array
             if (startsWithPusmin(str) && !multidivCheck(str)) { // str starts with +-
                 var arr = str.split(exp).filter(r => {return r.length !=0 }) // filters "" out of array
-                arr.splice(0,2,arr[0]+arr[1]) // ["-","2"] >>> ["-2"]
-                console.log("starts with +-: ",arr);
+                arr.splice(0,2,arr[0]+arr[1]) // ["-","2"] >>> ["-2"]                
         
             } else if (multidivCheck(str) && !startsWithPusmin(str)){ // string contains /-/+ *-*+
                 var arr = str.split(exp).filter(r => {return r.length !=0 }) // filters "" out of array and splits in operators
@@ -119,8 +99,7 @@ keys.addEventListener("click", e =>{
                     if (multiDiv(arr[i]) && plusMin(arr[i+1])) { //if i is */ AND i+1 is +-
                         arr.splice(i+1,2,arr[i+1] + arr[i+2])
                     }
-                }
-                console.log("multiDiv check> ",arr);
+                }                
             }else if (multidivCheck(str) && startsWithPusmin(str)) { // str contains /-/+ *-*+ AND starts with +-
                 var arr = str.split(exp).filter(r => {return r.length !=0 }) // filters "" out of array
                 arr.splice(0,2,arr[0]+arr[1]) // ["-","2"] >>> ["-2"]
@@ -128,39 +107,28 @@ keys.addEventListener("click", e =>{
                     if (multiDiv(arr[i]) && plusMin(arr[i+1])) { //if i is */ AND i+1 is +-
                         arr.splice(i+1,2,arr[i+1] + arr[i+2])
                     }
-                }
-                console.log("/-/+ *-*+ AND starts with +-: ",arr);
+                }                
             }else { // no +*+/ or -*-/
-                var arr = str.split(exp).filter(r => {return r.length !=0})
-                console.log("no +*+/ or -*-/: ",arr);
+                var arr = str.split(exp).filter(r => {return r.length !=0})                
             }
-            while (multiDiv(arr)) { // while calc contains */:
-                console.log("arr contains multidiv ")
+            while (multiDiv(arr)) { // while calc contains */:                
                 for (let q = 0; q<arr.length; ++q){
-                    if (multiDiv(arr[q])){ // if arr[q] is */ and arr[1+1] is not +-:
-                        console.log("*/ detected:: ", arr[q]);
+                    if (multiDiv(arr[q])){ // if arr[q] is */ and arr[1+1] is not +-:                        
                         // let res = calculate(arr[q-1],arr[q],arr[q+1])
                         var ress = calculate(arr[q-1],arr[q],arr[q+1])
                         arr.splice(q-1,3,ress);// use splice() method to replace q-1,q and q+1 with result
-                        // arr.splice(q-1,3,res);// use splice() method to replace q-1,q and q+1 with result
-                        console.log("arr after splice: ",arr);
+                        // arr.splice(q-1,3,res);// use splice() method to replace q-1,q and q+1 with result                        
                         break
                     } 
                 }
-            }
-            console.log("end of */ while loop ",arr);
-            while (plusMin(arr)) { // while +- in arr
-                console.log("plusmin detected with arr> ",arr);
+            }            
+            while (plusMin(arr)) { // while +- in arr                
                 for (let q = 0; q<arr.length; ++q){
-                    if (plusMin(arr[q]) && arr[q].length ===1){
-                        console.log("+- detected:: ", arr[q]);
+                    if (plusMin(arr[q]) && arr[q].length ===1){                        
                         // let res = calculate(arr[q-1],arr[q],arr[q+1]) //result
                         // arr.splice(q-1,3,res);// use splice() method to replace q-1,q and q+1 with result
                         var ress = calculate(arr[q-1],arr[q],arr[q+1])
-                        arr.splice(q-1,3,ress);// use splice() method to replace q-1,q and q+1 with result
-                        console.log("arr after splice: ",arr);
-                        console.log(arr.length);
-                        console.warn(res);
+                        arr.splice(q-1,3,ress);// use splice() method to replace q-1,q and q+1 with result                                                                        
                         break
                     }
         
@@ -169,17 +137,23 @@ keys.addEventListener("click", e =>{
                     break
                 }
             }
-        }else {
-            console.log("no calc detected")
-        } // end of calculations
-        
-        // console.warn("end of calc: ",ress, typeof ress);
-        if (typeof ress != "undefined" && isNaN(ress) === false ){ // calc finished
-            console.warn("res> ",ress);
-            console.error(isNaN(ress));
+        }
+
+        if (typeof ress != "undefined" && isNaN(ress) === false ){ // calc finished                        
 
             curr.textContent = ress
         }
     } 
-    
+    // while (display.scrollLeft < 0){
+
+    //     btn.textContent = "scroll to end"
+    //     btn.addEventListener("click", () =>{
+    //         display.scrollLeft = display.scrollWidth + 1
+    //     })
+    //     display_main.appendChild(btn)
+    //     break   
+    // }
+    // if (display.scrollLeft === 0 ){
+    //     btn.remove()
+    // }
 })
